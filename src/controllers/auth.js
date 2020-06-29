4d5e84b73a8272f258b787d4999f806f9bf6db68
Jademir de Moura <jademirf@gmail.com>
2020-06-29 16:20:51 -0300

    salvando dados no accounts do banco

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,13 +1,24 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
+const { Account } = require('../models')
 
 const router = express.Router()
+
+const saltRounds = 10
 
 router.get('/sign-in', (req, res) => {
   return res.json('Sign in')
 })
 
-router.get('/sign-up', (req, res) => {
-  return res.json('Sign up')
+router.get('/sign-up', async (req, res) => {
+
+  const user = {
+    email:'jademir@teste.com',
+    password: bcrypt.hashSync('123456', saltRounds)
+  }
+  const result = await Account.create(user)
+
+  return res.json(result)
 })
 
 module.exports = router
