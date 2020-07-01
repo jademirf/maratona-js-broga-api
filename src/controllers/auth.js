f38925a2538f050b79553d0eef252bcff2261ec5
Jademir de Moura <jademirf@gmail.com>
2020-07-01 12:53:44 -0300

    helper de validação com @hapi/joi

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,21 +1,23 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
 const { Account } = require('../models')
+const { accountSignUp } = require('../validators/account')
+const { getMessage } = require('../helpers/validator')
 
 const router = express.Router()
 
 const saltRounds = 10
 
-router.get('/sign-in', (req, res) => {
-  return res.json('Sign in')
+router.post('/sign-in', (req, res) => {
+  return res.jsonOK(null)
 })
 
-router.post('/sign-up', async (req, res) => {
+router.post('/sign-up', accountSignUp, async (req, res) => {
 
   const user = req.body
 
   const account = await Account.findOne({ where: { email: user.email } })
-  if (account) return res.jsonBadRequest(null, 'Account already exists')
+  if (account) return res.jsonBadRequest(null, getMessage('account.signup.email_exists'))
 
   user.password = bcrypt.hashSync(req.body.password, saltRounds)
 
@@ -25,7 +27,7 @@ router.post('/sign-up', async (req, res) => {
   // }
   const result = await Account.create(user)
 
-  return res.jsonOK(result, 'Your account has been created!')
+  return res.jsonOK(result, getMessage('account.signup.success'))
 })
 
 module.exports = router
